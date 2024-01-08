56443ba8217695cba1a4dc22ef20131abaf4ec70
hyunjin_byun <shinbm219@naver.com>
2024-01-08 23:53:35 +0900

    [섹션5] 스í프링mvc 도입

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,16 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+//스피링부트 3.0이상부터는 Component랑 RequestMapping으로 스프링빈에 저장 안됨.
+@Controller
+public class SpringMemberFormControllerV1 {
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
